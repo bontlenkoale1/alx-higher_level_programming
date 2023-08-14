2970a3f77324e2d2c7aaeefc55387ad2192ecc19
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-08-14 13:26:42 -0700

    13-is_palindrome.c, lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdlib.h>
 /**
  * struct listint_s - singly linked list
@@ -21,16 +22,5 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-def print_list_integer(my_list=[]):
-def element_at(my_list, idx):
-def replace_in_list(my_list, idx, element):
-def print_reversed_list_integer(my_list=[]):
-def new_in_list(my_list, idx, element):
-def no_c(my_string):
-def print_matrix_integer(matrix=[[]]):
-def add_tuple(tuple_a=(), tuple_b=()):
-def multiple_returns(sentence):
-def max_integer(my_list=[]):
-def divisible_by_2(my_list=[]):
-def delete_at(my_list=[], idx=0):
-#endif
+#endif /* LISTS_H */
+
