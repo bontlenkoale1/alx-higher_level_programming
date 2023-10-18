df2b707d00b9c2f5a2198bfe258ad59a5d98193a
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-10-18 14:51:40 -0700

    SQL_more_queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,6 @@
--- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
--- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
-SELECT s.`title`, g.`genre_id`
-  FROM `tv_shows` AS s
-        INNER JOIN `tv_show_genres` AS g
-	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are sorted in ascending ordered by cities.id.
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
